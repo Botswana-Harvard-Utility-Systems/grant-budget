31bc38f9cef89441fc15964594fbb60c16af6cdd
Moses Chawawa <moseschawawa@gmail.com>
2021-07-04 10:09:27 +0200

    first commit before refactoring

---
+++ apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class PersonnelConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'grant_budget'
+    app_label = 'grant_budget'
