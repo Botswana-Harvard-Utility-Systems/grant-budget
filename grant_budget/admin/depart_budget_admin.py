e432c03cf62d674afae7c029d055ef10a251fa1c
Moses Chawawa <moseschawawa@gmail.com>
2021-07-13 14:20:07 +0200

    Model relationship fix

--- grant_budget/admin/depart_budget_admin.py
+++ grant_budget/admin/depart_budget_admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from ..models import DepartmentBudget
+from ..models import DepartmentBudget, Budget
 from ..forms import DepartmentBudgetForm
 from ..main_admin import grand_budget_admin
 
 
+# @admin.register(Budget, site=grand_budget_admin)
 class BudgetAdmin(admin.TabularInline):
-    pass
+    model = Budget
 
 
 @admin.register(DepartmentBudget, site=grand_budget_admin)
 class DepartmentBudgetAdmin(admin.ModelAdmin):
     form = DepartmentBudgetForm
+    inlines = [BudgetAdmin, ]
--- grant_budget/models/grant.py
+++ grant_budget/models/grant.py
@@ -1,5 +1,4 @@
 from django.db import models
-from .dept_budget import DepartmentBudget
 
 
 class Grant(models.Model):
@@ -7,4 +6,3 @@ class Grant(models.Model):
     name = models.CharField(max_length=50)
     start = models.DateField()
     end = models.DateField()
-    department_budgets = models.ForeignKey(DepartmentBudget, on_delete=models.CASCADE)
