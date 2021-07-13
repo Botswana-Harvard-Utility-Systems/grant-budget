5647a43af5ce7ec61154af77b393a615e60b1b11
Moses Chawawa <moseschawawa@gmail.com>
2021-07-13 14:03:47 +0200

    Tests initialization [awaiting business logic]

--- grant_budget/admin/depart_budget_admin.py
+++ grant_budget/admin/depart_budget_admin.py
@@ -4,6 +4,10 @@ from ..forms import DepartmentBudgetForm
 from ..main_admin import grand_budget_admin
 
 
+class BudgetAdmin(admin.TabularInline):
+    pass
+
+
 @admin.register(DepartmentBudget, site=grand_budget_admin)
 class DepartmentBudgetAdmin(admin.ModelAdmin):
     form = DepartmentBudgetForm
