120eb4782120aba077af10e4252d74f918537b98
Coulson Kgathi <ckgathi@gmail.com>
2021-07-14 13:53:11 +0200

    trivial

--- grant_budget/main_admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-
-class GrandBudgetAdmin(admin.AdminSite):
-    site_title = "Grant Budget Management System"
-    site_header = site_title
-
-
-grand_budget_admin = GrandBudgetAdmin(name='grand_budget_admin')
