8dba45c16d40c35e97f74b67d871e8c0adc37743
Moses Chawawa <moseschawawa@gmail.com>
2021-07-15 11:06:50 +0200

    model relationship fix

--- grant_budget/models/allowance.py
+++ grant_budget/models/allowance.py
@@ -1,8 +1,15 @@
 from django.db import models
 from edc_base.model_mixins import BaseUuidModel
+from .salary import Salary
 
 
 class Allowance(BaseUuidModel):
+    salary = models.ForeignKey(
+        Salary,
+        verbose_name='Salary',
+        on_delete=models.CASCADE
+    )
+
     name = models.CharField(
         verbose_name="Name",
         max_length=10,
